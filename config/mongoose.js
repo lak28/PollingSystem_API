b1ed255d044abe19e6c5a869839744e3b29d9fe2
lak28 <89638544+lak28@users.noreply.github.com>
2022-03-08 20:50:13 +0530

    Connected to mongoDB

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,3 +1,5 @@
+//Database configuration file
+
 const mongoose = require("mongoose");
 //connecting mongoose with database
 mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://lak2896:12345@cluster0.92kkk.mongodb.net/PollingSystem_DB?retryWrites=true&w=majority");
